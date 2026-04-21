224ad6648683483600aa0746c5c3a6d875dd0ef5
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2026-04-21 13:01:45 -0700

    refactor to fix ammo/item properties, improve typing

--- tools/1-fix_tdev_format.py
+++ tools/1-fix_tdev_format.py
@@ -3,34 +3,34 @@
 from typing import Any
 
 from env import TARKOV_DEV_FILES, TMP_DIR
+from item_types import MongoID
 from utils import json_dump, json_load
 
-AMMO: list[dict[str, Any]] = json_load(TARKOV_DEV_FILES / "ammo.json")
-ITEMS: list[dict[str, Any]] = json_load(TARKOV_DEV_FILES / "items.json")
+ItemProperties = dict[str, Any]
+
+AMMO: list[ItemProperties] = json_load(TARKOV_DEV_FILES / "ammo.json")
+ITEMS: list[ItemProperties] = json_load(TARKOV_DEV_FILES / "items.json")
 
 FIXED_ITEMS_F = TMP_DIR / "items.json"
 
+ItemsDict = dict[MongoID, ItemProperties]
 
-def merge_ammo_items():
-    items: dict[str, dict[str, Any]] = {i["id"]: i for i in ITEMS}
-    for a in AMMO:
+
+def merge_ammo_items(
+    old_items: list[ItemProperties], ammo: list[ItemProperties]
+) -> ItemsDict:
+    items: ItemsDict = {i["id"]: i for i in old_items}
+    for a in ammo:
         ref_item: dict[str, str] = a.pop("item")
         item = items[ref_item["id"]]
 
         for prop, value in a.items():
             item[prop] = value
 
-    json_dump(items, FIXED_ITEMS_F)
+    return items
 
 
-def simplify_conflicting_items():
-    key = "ConflictingItems"
-    for item in ITEMS:
-        simplified = [i["id"] for i in item[key]]
-        item[key] = simplified or []
-
-
-def convert_keys():
+def convert_keys(items: ItemsDict):
     """Replace all keys with SPT DB equivalents."""
     names = {
         "weight": "Weight",
@@ -50,20 +50,29 @@ def convert_keys():
         "stackMaxSize": "StackMaxSize",
     }
 
-    for ind, item in enumerate(ITEMS):
-        new_item: dict[str, Any] = {}
-        for k, v in item.items():
+    for mid, props in items.items():
+        new_props: ItemProperties = {}
+        for k, v in props.items():
             new_key = names.get(k, k)
-            new_item[new_key] = v
-        ITEMS[ind] = new_item
+            new_props[new_key] = v
+        items[mid] = new_props
+
+
+def simplify_conflicting_items(items: ItemsDict):
+    key = "ConflictingItems"
+    for item in items.values():
+        simplified = [i["id"] for i in item[key]]
+        item[key] = simplified or []
 
 
 def main():
     TMP_DIR.mkdir(parents=True, exist_ok=True)
 
-    convert_keys()
-    simplify_conflicting_items()
-    merge_ammo_items()
+    items = merge_ammo_items(ITEMS, AMMO)
+    convert_keys(items)
+    simplify_conflicting_items(items)
+
+    json_dump(items, FIXED_ITEMS_F)
 
 
 if __name__ == "__main__":
