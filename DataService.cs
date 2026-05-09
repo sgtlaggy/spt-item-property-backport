0708c92a97e84e4d61d5fcf9466d92df279619d8
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2026-05-09 14:59:36 -0700

    automatically download ‘items.json’ from repo if file does not exist

--- DataService.cs
+++ DataService.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Utils;
 
 namespace ItemPropertyBackport;
@@ -10,20 +11,23 @@ namespace ItemPropertyBackport;
 public class DataService
 {
     private JsonUtil _json;
+    private ISptLogger<DataService> _logger;
 
     private string modDir;
     private string configFile;
-    private string itemsFile;
+    private string changesFile;
+    private string changesUrl = "https://raw.githubusercontent.com/sgtlaggy/spt-item-property-backport/refs/heads/master/Resources/db/items.json";
 
     private Config? config;
 
-    public DataService(JsonUtil json, ModHelper modHelper)
+    public DataService(JsonUtil json, ModHelper modHelper, ISptLogger<DataService> logger)
     {
         _json = json;
+        _logger = logger;
 
         modDir = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
         configFile = Path.Join(modDir, "config.json");
-        itemsFile = Path.Join(modDir, "db", "items.json");
+        changesFile = Path.Join(modDir, "db", "items.json");
 
         config = json.DeserializeFromFile<Config>(configFile);
     }
@@ -40,13 +44,58 @@ public class DataService
 
     public async Task<Dictionary<MongoId, ItemProperties>> GetItemChanges()
     {
-        var items = await _json.DeserializeFromFileAsync<Dictionary<MongoId, ItemProperties>>(itemsFile);
+        var config = GetConfig();
 
-        if (items is null)
+        Dictionary<MongoId, ItemProperties>? changes = null;
+        if (!File.Exists(changesFile))
+        {
+            _logger.Warning("[ItemPropertyBackport] Item file not found, redownloading.");
+            var response = await GetWithRetries(changesUrl, 2);
+            if (response is not null)
+            {
+                changes = _json.Deserialize<Dictionary<MongoId, ItemProperties>>(response);
+
+                var directory = Path.GetDirectoryName(changesFile)!;
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                await File.WriteAllTextAsync(changesFile, response);
+            }
+        }
+        else
+        {
+            changes = await _json.DeserializeFromFileAsync<Dictionary<MongoId, ItemProperties>>(changesFile);
+        }
+
+        if (changes is null)
         {
             throw new Exception("Failed to load item changes.");
         }
 
-        return items;
+        return changes;
+    }
+
+    // Adapted from LiveFleaPrices’ ‘GetWithRetries’ method
+    // Moved loop into the ‘using’ block and changed mod name in log.
+    private async Task<string?> GetWithRetries(string url, int retries)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            for (var i = 0; i <= retries; i++)
+            {
+                try
+                {
+                    return await client.GetStringAsync(url);
+                }
+                catch (Exception e)
+                {
+                    _logger.Error($"[ItemPropertyBackport] Error downloading JSON, attempt {i + 1}/{retries + 1}: {e.Message}");
+                }
+            }
+        }
+
+        return null;
     }
 }
