a50c39e1b75852c13812b29f57a7af97aa09e1d2
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2026-05-22 12:20:25 -0700

    [scripts] refactor the refactor script to use classes

--- tools/2-refactor_data.py
+++ tools/2-refactor_data.py
@@ -1,82 +1,123 @@
 #!/usr/bin/env python3
 
-from typing import Any
+from dataclasses import dataclass, field
+from queue import PriorityQueue
+from typing import Any, Callable, TypeVar
 
 from env import TARKOV_DEV_FILES, TMP_DIR
 from models import MongoID
 from utils import hang, json_dump, json_load
 
-ItemProperties = dict[str, Any]
-ItemsDict = dict[MongoID, ItemProperties]
+T = TypeVar("T")
+
+Method = Callable[[T], None]
+PropDict = dict[str, Any]
 
 
-def move_generic_properties(items: ItemsDict):
-    ignore_keys = {"id", "types", "properties"}
+# dataclass with ‘order’ and ‘comapre’ for PriorityQueue sorting
+@dataclass(order=True)
+class FixerMethod:
+    priority: int
+    method: Method = field(compare=False)
 
-    props_to_move: list[str] = []
-    for item in items.values():
-        for key in item.keys():
-            if key not in ignore_keys:
-                props_to_move.append(key)
-
-        break  # use only the first item to harvest names of properties to move
-
-    for item in items.values():
-        props: dict[str, Any] | None = item["properties"]
-        if props is None:
-            item["properties"] = props = {}
-
-        for prop in props_to_move:
-            props[prop] = item.pop(prop)
+    def __call__(self, *args, **kwargs):
+        self.method(*args, **kwargs)
 
 
-def fix_percentages(items: ItemsDict):
-    properties = [
-        "weaponErgonomicPenalty",
-        "speedPenaltyPercent",
-        "mousePenalty",
-        "CheckTimeModifier",
-        "LoadUnloadModifier",
-    ]
-    for item in items.values():
-        props: dict[str, Any] = item["properties"]
-        for prop in properties:
-            val: float | None = props.get(prop)
-            if val:  # not 0 or null
-                props[prop] = round(props[prop] * 100, 2)
+def fixer(priority: int = 0) -> Callable[[Method], FixerMethod]:
+    def decorator(method: Method) -> FixerMethod:
+        """Decorator that marks a method to be run by ."""
+        return FixerMethod(priority, method)
+
+    return decorator
 
 
-def set_flea_blacklist(items: ItemsDict):
-    for item in items.values():
-        blacklisted = "noFlea" in item["types"]
-        item["properties"]["CanSellOnRagfair"] = not blacklisted
+class DataFixer:
+    filename: str
+
+    data: dict[MongoID, PropDict]
+
+    def load(self):
+        lst: list[PropDict] = json_load(TARKOV_DEV_FILES / self.filename)
+        self.data = {thing["id"]: thing for thing in lst}
+
+    def save(self):
+        json_dump(self.data, TMP_DIR / self.filename)
+
+    def fix(self):
+        if not hasattr(self, "data"):
+            self.load()
+
+        fixers: PriorityQueue[FixerMethod] = PriorityQueue()
+        for attr in type(self).__dict__.values():
+            if isinstance(attr, FixerMethod):
+                fixers.put(attr)
+        while not fixers.empty():
+            fixer = fixers.get()
+            fixer(self)
+
+        self.save()
 
 
-def simplify_conflicting_items(items: ItemsDict):
-    # [{id: XXX}] -> [XXX]
-    for item in items.values():
-        conflicting_items = item["properties"]["ConflictingItems"]
-        conflicting_items[:] = [i["id"] for i in conflicting_items]
+class ItemFixer(DataFixer):
+    filename = "items.json"
 
+    @fixer(1)
+    def move_generic_properties(self):
+        ignore_keys = {"id", "types", "properties"}
 
-def fix_items():
-    input_file = TARKOV_DEV_FILES / "items.json"
-    output_file = TMP_DIR / "items.json"
+        props_to_move: list[str] = []
+        for item in self.data.values():
+            for key in item.keys():
+                if key not in ignore_keys:
+                    props_to_move.append(key)
 
-    raw_items: list[ItemProperties] = json_load(input_file)
-    items = {item["id"]: item for item in raw_items}
+            break  # use only the first item to harvest names of properties to move
 
-    move_generic_properties(items)
-    fix_percentages(items)
-    set_flea_blacklist(items)
-    simplify_conflicting_items(items)
+        for item in self.data.values():
+            # This looks like a place for ‘dict.setdefault’ but
+            # the key is always present, just null in some cases.
+            props: PropDict | None = item["properties"]
+            if props is None:
+                item["properties"] = props = {}
 
-    TMP_DIR.mkdir(parents=True, exist_ok=True)
-    json_dump(items, output_file)
+            for prop in props_to_move:
+                props[prop] = item.pop(prop)
+
+    @fixer(2)
+    def fix_percentages(self):
+        properties = [
+            "weaponErgonomicPenalty",
+            "speedPenaltyPercent",
+            "mousePenalty",
+            "CheckTimeModifier",
+            "LoadUnloadModifier",
+        ]
+        for item in self.data.values():
+            props: PropDict = item["properties"]
+            for prop in properties:
+                val: float | None = props.get(prop)
+                if val:  # not 0 or null
+                    props[prop] = round(props[prop] * 100, 2)
+
+    @fixer(3)
+    def set_flea_blacklist(self):
+        for item in self.data.values():
+            blacklisted = "noFlea" in item["types"]
+            item["properties"]["CanSellOnRagfair"] = not blacklisted
+
+    @fixer(4)
+    def simplify_conflicting_items(self):
+        # [{id: XXX}] -> [XXX]
+        for item in self.data.values():
+            conflicting_items = item["properties"]["ConflictingItems"]
+            conflicting_items[:] = [i["id"] for i in conflicting_items]
 
 
 def main():
-    fix_items()
+    TMP_DIR.mkdir(parents=True, exist_ok=True)
+
+    ItemFixer().fix()
 
 
 if __name__ == "__main__":
