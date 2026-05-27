38c0460b2a9b0d2537080cc0b1abee2919fab677
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2026-05-26 18:11:27 -0700

    [scripts] use json encoder to force sorting lists and dicts

--- tools/utils.py
+++ tools/utils.py
@@ -1,7 +1,19 @@
-import sys
 import json
+import sys
 from pathlib import Path
 from typing import Any
+
+
+class SortedJsonEncoder(json.JSONEncoder):
+    def __init__(self, *args, **kwargs):
+        kwargs.pop("sort_keys")
+        super().__init__(*args, **kwargs, sort_keys=True)
+
+    def encode(self, o: Any) -> str:
+        if o and isinstance(o, list) and isinstance(o[0], (str, float, int)):
+            o.sort()
+
+        return super().encode(o)
 
 
 def json_load(fp: Path) -> Any:
@@ -9,9 +21,9 @@ def json_load(fp: Path) -> Any:
 
 
 def json_dump(obj: Any, fp: Path):
-    fp.write_text(json.dumps(obj, indent='\t'))
+    fp.write_text(json.dumps(obj, indent="\t", cls=SortedJsonEncoder))
 
 
 def hang():
     if not sys.argv[1:]:
-        input('Press RETURN to exit.')
+        input("Press RETURN to exit.")
